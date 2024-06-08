1334d5f8a357d723d90ecf748abb67d628682172
johann <johannzr17@gmail.com>
2024-06-07 23:45:36 -0500

    Establishment details view and personal information view

--- app/src/main/java/com/latinka/tinkawork/account/ui/screens/PersonalInformationActivity.kt
+++ app/src/main/java/com/latinka/tinkawork/account/ui/screens/PersonalInformationActivity.kt
@@ -1,4 +1,4 @@
-package com.latinka.tinkawork.auth.ui.screens
+package com.latinka.tinkawork.account.ui.screens
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.latinka.tinkawork.R
 
-class LoginActivity : AppCompatActivity() {
+class PersonalInformationActivity: AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_personal_information)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
