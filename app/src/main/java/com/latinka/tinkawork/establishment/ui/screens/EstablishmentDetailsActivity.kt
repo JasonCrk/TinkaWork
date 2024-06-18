8a37d18580460dcfa413f8171de901073120ccea
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2024-06-18 02:26:45 -0500

    fix: Update views

--- app/src/main/java/com/latinka/tinkawork/establishment/ui/screens/EstablishmentDetailsActivity.kt
+++ app/src/main/java/com/latinka/tinkawork/establishment/ui/screens/EstablishmentDetailsActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 import com.latinka.tinkawork.R
 
 class EstablishmentDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_establishment_details)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
