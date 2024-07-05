a66dfebd9cca6f2ad62d2ba63cdbbb5c45b0083c
Eduardo <i202213895@cibertec.edu.pe>
2024-07-05 01:45:30 -0500

    Feat:Class(Account Options, Change Password, Personal Information); Views(account options, change password, personal information); add(incon_email)

--- app/src/main/java/com/latinka/tinkawork/account/ui/screens/AccountOptionsScreenFragment.kt
+++ app/src/main/java/com/latinka/tinkawork/account/ui/screens/AccountOptionsScreenFragment.kt
@@ -7,9 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 
 import com.latinka.tinkawork.R
 import com.latinka.tinkawork.auth.ui.screens.LoginActivity
@@ -18,7 +24,8 @@ import com.latinka.tinkawork.databinding.FragmentAccountOptionsScreenBinding
 class AccountOptionsScreenFragment : Fragment() {
 
     private lateinit var binding: FragmentAccountOptionsScreenBinding
-
+    private   lateinit var db : FirebaseFirestore
+    private lateinit var auth : FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,32 +37,65 @@ class AccountOptionsScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.apply {
-            personalInformationBtn.setOnClickListener {
+        auth = Firebase.auth
+        val db = Firebase.firestore
+        val currentUser = auth.currentUser
+        val uidUser = currentUser?.uid
+        val userRef = uidUser?.let { db.collection("users").document(it) }
+
+        val txtFirstname = binding.accountFirstName
+        val txtLastName = binding.accountLastName
+        val txtRoles = binding.accountRole
+        userRef?.get()
+            ?.addOnSuccessListener { documentSnapshot->
+                if(documentSnapshot.exists()){
+                    txtFirstname.text = documentSnapshot.getString("first_name")
+                    txtLastName.text  = documentSnapshot.getString("last_name")
+                }
+                fun obtenerNombreRol(userId: String): Task<String> {
+                    val userRef = db.collection("users").document(userId)
+                    return userRef.get().continueWithTask { task ->
+                        val rolRef = task.result?.get("role") as? DocumentReference?
+                        if (rolRef != null) {
+                            rolRef.get().continueWith { rolTask ->
+                                rolTask.result?.getString("name") ?: "Rol no encontrado"
+                            }
+                        } else {
+                            Tasks.forResult("Rol no asignado")
+                        }
+                    }
+                }
+                obtenerNombreRol(uidUser).addOnSuccessListener { rol ->
+                    txtRoles.text = rol
+                }
             }
+        binding.personalInformationBtn.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.nav_container, PersonalInformationScreenFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.btnChangePassword.setOnClickListener{
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.nav_container, ChangePasswordScreenFragment())
+                .addToBackStack(null)
+                .commit()
+        }
 
-            changePasswordBtn.setOnClickListener {
-                parentFragmentManager.beginTransaction()
-                    .replace(R.id.nav_container, ChangePasswordScreenFragment.newInstance())
-                    .addToBackStack(null)
-                    .commit()
-            }
+        binding.signOutBtn.setOnClickListener {
+            binding.signOutBtn.isEnabled = false
+            binding.personalInformationBtn.isEnabled = false
 
-            signOutBtn.setOnClickListener {
-                binding.signOutBtn.isEnabled = false
-                binding.personalInformationBtn.isEnabled = false
+            binding.signOutBtn.background.setTint(
+                ContextCompat.getColor(requireContext(), R.color.gray)
+            )
 
-                binding.signOutBtn.background.setTint(
-                    ContextCompat.getColor(requireContext(), R.color.gray)
-                )
+            Firebase.auth.signOut()
 
-                Firebase.auth.signOut()
-
-                startActivity(
-                    Intent(requireContext(), LoginActivity::class.java)
-                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                )
-            }
+            startActivity(
+                Intent(requireContext(), LoginActivity::class.java)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            )
         }
     }
 
